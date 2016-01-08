da22eca66df67673a6ecd266c6839ed1a6518578
Udit Desai <desaiuditd@gmail.com>
2016-01-07 17:02:13 -0800

    adds styles & loads existing todos

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ $( function() {
 
 	// to-do Model
 	var ToDoItem = Backbone.Model.extend( {
-		initialize: function() {
-			this.category = '';
-		},
 		parse: function( item ) {
+			var self = this;
 			Backbone.ajax( {
 				dataType: 'json',
 				method: 'get',
 				url: ToDoApiUrl + 'todos/' + item.id + '/categories/',
 				success: function( categories ) {
 					if ( categories.length > 0 ) {
-						category = categories[0];
-						this.category = category.name;
+						var category = categories[0];
+						self.set( { category: category.name } );
+						self.collection.trigger( 'updateCategory' );
 					}
+					console.log(self);
 				}
 			} );
 			return item;
@@ -30,7 +30,16 @@ $( function() {
 	// To-Do Collection
 	var ToDoList = Backbone.Collection.extend( {
 		model: ToDoItem,
-		url: ToDoApiUrl + 'todos/'
+		url: ToDoApiUrl + 'todos/',
+		resetTodos: function() {
+			var self = this;
+			this.fetch( {
+				reset: true,
+				success: function( collection, response, options ) {
+					self.trigger( 'successOnFetch' );
+				}
+			} );
+		}
 	} );
 
 	var todos = new ToDoList;
@@ -38,27 +47,47 @@ $( function() {
 	var ToDoItemView = Backbone.View.extend( {
 		tagName: 'li',
 		className: 'todo-item',
+		template: _.template( $( '#item-template' ).html() ),
 		render: function() {
-
+			this.$el.html( this.template( this.model.toJSON() ) );
+			//this.$el.toggleClass( 'done', this.model.get( 'done' ) );
+			this.input = this.$( '.edit' );
+			return this;
 		}
 	} );
 
 	var ToDoListView = Backbone.View.extend( {
 		id: 'todo-list',
 		render: function() {
-
+			var self = this;
+			_.each( this.collection.models, function( item, key ) {
+				self.$el.append( item.render().el );
+			} );
 		}
 	} );
 
 	var AppView = Backbone.View.extend( {
 		id: 'todo-app',
 		initialize: function() {
+			this.todolistel = $( '#todo-list' );
 			//this.listenTo( todos, 'change',  )
 			//this.listenTo( todos, 'all', this.render );
-			todos.fetch();
-		},
-		render: function() {
 
+			// fetch the existing data from API. This is coming via ajax from API, since we don't have server side script to render it on page load.
+			todos.resetTodos();
+
+			this.listenTo( todos, 'successOnFetch', this.render );
+			this.listenTo( todos, 'updateCategory', this.updateCategory );
+		},
+		render: function( options ) {
+			todos.each( this.addOne, this );
+		},
+		addOne: function( todo ) {
+			var view = new ToDoItemView( { model: todo } );
+			this.todolistel.append( view.render().el );
+		},
+		updateCategory: function() {
+			console.log(todos.models);
 		}
 	} );
 
